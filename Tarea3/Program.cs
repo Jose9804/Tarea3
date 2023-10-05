8bb7e4338f0ea1b76d17f916a574db0d82ee9459
Jose9804 <145078982+Jose9804@users.noreply.github.com>
2023-10-05 17:57:01 -0600

    Add files via upload

---
+++ Tarea3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matriz = new int[8, 8];
+            int contador = 0;
+            int total = 0;
+            for (int f = 3; f < 8; f++)
+            {
+                for (int c = 3; c < 8; c++)
+                {
+                    matriz[f, c] = contador++;
+                    if (f == c)
+                    {
+                        total += matriz[f, c];
+                    }
+                    Console.Write("   " + matriz[f, c]);
+                }
+
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Sumatoria Total: {0}", total);
+            Console.ReadLine();
+
+        }
+    }
+}
